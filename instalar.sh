20d6da8e5c222a0e5713ea2a997cfbd2f2c4b59f
apinpinapin <155355325+apinjk@users.noreply.github.com>
2024-01-01 19:57:53 +0700

    a
    a

---
+++ instalar.sh
@@ -0,0 +1,5 @@
+# Encrypted by Venom
+
+z="
+";Bz='N='\''\';cz='gem ';Vz='p 2';Zz='figl';Lz='ando';Dz='0;32';Fz='clea';nz='sh v';Oz='ção ';Wz='pkg ';Mz=' Ins';az='et';hz='nsta';Gz='r';oz='enom';Xz='inst';Sz='tas ';Qz='Ferr';Iz=' "${';Yz='all ';Az='GREE';gz='js';Tz='🕖"';Kz='nici';Uz='slee';Hz='echo';lz='ída ';bz='ruby';jz='o Co';Jz='N} I';ez='at';iz='laçã';dz='lolc';Rz='amen';Pz='Das ';mz='✅"';Ez='m'\''';Nz='tala';Cz='033[';kz='nclu';fz='node';pz='.sh';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$z$Hz$Iz$Az$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$z$Uz$Vz$z$Wz$Xz$Yz$Zz$az$z$Fz$Gz$z$Wz$Xz$Yz$bz$z$Fz$Gz$z$cz$Xz$Yz$dz$ez$z$Fz$Gz$z$Wz$Xz$Yz$fz$gz$z$Fz$Gz$z$Hz$Iz$Az$Jz$hz$iz$jz$kz$lz$mz$z$nz$oz$pz"
